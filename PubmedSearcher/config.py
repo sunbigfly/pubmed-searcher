ec1eb54cd632665b4e588b5b069c9e7f4e80736f
bigfly sun <58769230+sunbigfly@users.noreply.github.com>
2024-07-03 17:00:47 +0800

    Add files via upload

---
+++ PubmedSearcher/config.py
@@ -0,0 +1,15 @@
+import os
+from datetime import timedelta
+
+
+class Config:
+    SECRET_KEY = 'sunshinesunbigfly@gmail.com'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tmp', 'test.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REMEMBER_COOKIE_DURATION = timedelta(days=30)
+    CACHE_TYPE = 'redis'
+    CACHE_REDIS_HOST = 'localhost'
+    CACHE_REDIS_PORT = 6379
+    CACHE_REDIS_DB = 0
+    CACHE_REDIS_URL = 'redis://localhost:6379/0'
+    CACHE_DEFAULT_TIMEOUT = 60 * 60 * 24 * 30 * 12  # 缓存过期时间为1年
