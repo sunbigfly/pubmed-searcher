530841b46f1d6a5f9ee6db81999f48640129c61a
bigfly sun <58769230+sunbigfly@users.noreply.github.com>
2024-07-04 09:18:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 
 class Config:
-    SECRET_KEY = 'sunshinesunbigfly@gmail.com'
+    SECRET_KEY = 'xxx'  # 填写自己的密码
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'tmp', 'test.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REMEMBER_COOKIE_DURATION = timedelta(days=30)
